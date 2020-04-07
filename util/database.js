c7c02b489573f50d916f0926594815a404dd5b36
ddelatycki1@my.bcit.ca <ddelatycki1@my.bcit.ca>
2020-04-06 19:07:17 -0700

    changed db credentials

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({  
-      host: 'ec2-3-211-48-92.compute-1.amazonaws.com',  
-      user: 'lvyyceazjfxluk',  
-      database: 'd3blmq4sbmqdd0',  
-      password: '8c8a7671883afcd3261630a921bac9f941b7d696d319254f83504aa8fb70175b',
+      host: 'ec2-52-71-55-81.compute-1.amazonaws.com',  
+      user: 'vdpoeizmqhzwob',  
+      database: 'd820hdq7j0g7fi',  
+      password: '256ba8c144246cc4aac61fff37750dec8cde79cd855cbe305c7d99d2f48858b7',
       port: 5432,
       ssl: true
   });
